1f88769c7ced779079167448821b7962ea16f65f
NinjaStalin <kurisu@iki.fi>
2013-05-14 14:25:30 +0300

    somehow working version

--- public_html/js/randomizer.js
+++ public_html/js/randomizer.js
@@ -1,34 +1,121 @@
-function randomize() {
-    var n = $("#count")[0].value;
-    
-    createCandidates(10);
-}
+var settings = {
+    perPage: 24,
+	minLudumDare: 15
+};
 
-function createCandidates(amount) {
-    var area = $("#outputArea");
+
+function randomize() {
+    emptyOutput();
     
-    //$("#outputArea").empty();
-    
-    var numberOfCandidates = 5;
-    
-    for (var i=0; i<amount; ++i) {
-        var random = Math.floor((Math.random() * numberOfCandidates) + 1);
-        printCandidate(area, random);
+    var numberOfCandidates = getNumberOfCandidates();
+    var howMany = $("#howMany").val();
+	
+    for (var i = 0; i < howMany; ++i) {
+        var r = random(numberOfCandidates);
+        createCandidate($("#output"), r);
     }
 }
 
-function cleareElements(area) {
-    var elements = $("#outputArea").children();
+
+// Returns random integer between 0 and n-1
+function random(n) {
+	return Math.floor((Math.random() * n));
 }
 
 
-function printCandidate(area, number) {
-    var elem = document.createElement("p");
-    elem.appendChild(document.createTextNode(number));
-    area.add(elem);
+function createCandidate(area, number) {
+    var page = Math.floor(number / settings.perPage) + 1;
+    var game = number % settings.perPage + 1;
+	
+	var link = createLink(page);
+	var text = "game "+game;
+	
+    area.append("<p>"+link+" "+text+"</p>");
+}
+
+
+function createLink(page) {
+	var number = selectedLudumDare();
+	var start = (page - 1) * settings.perPage;
+	
+	var type = selectedRadioButton();
+	if (type === "all") type = "";
+	if (type === "jam") {
+		if (number >= 21)
+			type = "open";
+		else if (number >= 18 && number <= 20)
+			type = "gamejam";
+	}
+	
+	return "<a href=\"http://www.ludumdare.com/compo/ludum-dare-"+number+"/?action=preview&q=&etype="+type+"&start="+start+"\">page "+page+"</a>";
+}
+
+
+function getNumberOfCandidates() {
+	var selected = selectedRadioButton();
+	var i = selectedLudumDare();
+	
+	if (selected === "all")
+		return ludumEntries[i-1][0] + ludumEntries[i-1][1];
+	if (selected === "compo")
+		return ludumEntries[i-1][0];
+	if (selected === "jam")
+		return ludumEntries[i-1][1];
+}
+
+
+function selectedRadioButton() {
+	if ($("input[value='all']:checked").length > 0)
+		return "all";
+	else if ($("input[value='compo']:checked").length > 0)
+		return "compo";
+	else if ($("input[value='jam']:checked").length > 0)
+		return "jam";
+}
+
+
+function createDropDownSelect() {
+	for (var i = ludumEntries.length; i >= settings.minLudumDare; --i) {
+		$("#ludumDareNumber").append("<option value=\""+i+"\">"+i+"</option>");
+	}
+}
+
+
+function createRadioButtonTexts() {
+	emptyOutput();
+	
+	var i = selectedLudumDare();
+	var compoEntries = ludumEntries[i-1][0];
+	var jamEntries = ludumEntries[i-1][1];
+	
+	$("#labelAll").empty();
+	$("#labelAll").append("All Entries ("+ (compoEntries+jamEntries) +")");
+	$("#labelCompo").empty();
+	$("#labelCompo").append("48 Hour Compo Entries ("+ compoEntries +")");
+	$("#labelJam").empty();
+	$("#labelJam").append("Jam Entries ("+ jamEntries +")");
+	
+	$("#info").empty();
+	if (i < 18)
+		$("#info").append("<b>Note that before Ludum Dare 18 there was only competition.</b>");
+}
+
+
+function emptyOutput() {
+	$("#output").empty();
+}
+
+
+function selectedLudumDare() {
+	return $("#ludumDareNumber").val();
 }
 
 
 $(document).ready(function() {
-    $("#randomizeButton")[0].addEventListener("click", randomize);
+    createDropDownSelect();
+    createRadioButtonTexts();
+	
+	$("#ludumDareNumber").change(createRadioButtonTexts);
+	$("input[name='chooser']").change(emptyOutput);
+    $("#randomizeButton").click(randomize);
 });
